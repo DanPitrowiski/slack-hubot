3180aa5a27a64649b00980cff288889d7b724a91
Dan Pitrowiski <danpitrowiski@gmail.com>
2017-09-27 19:06:59 -0700

    Adding another food item

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -37,7 +37,7 @@ If you want to supplement your existing code above with any the code below,
 you need to move the contents of module.exports below into the module.exports code above.
 */
 var food;
-food = ["https://i.imgur.com/ayPsxfP.gifv"];
+food = ["https://i.imgur.com/ayPsxfP.gifv","http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg"];
 
 var squirrels;
 squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
