27e4c769e78f1eb4bf9a99efcdffafb7faa3d1ad
Dan Pitrowiski <danpitrowiski@gmail.com>
2017-10-01 17:22:42 -0700

    Fixed bot from not responding to some messages

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -34,24 +34,26 @@ EXAMPLES OF THE KEY HUBOT FUNCTIONS
 var food;
 food = ["https://i.imgur.com/ayPsxfP.gifv","http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg"];
 
-// var squirrels;
-// squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+var squirrels;
+squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
 module.exports = function(robot) {
-  // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!"
 
+  robot.hear(/food/i, function(msg) {
+    msg.send("Danbot v1.0");
+    return msg.send(msg.random(food));
+  });
 
-  return robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
+  robot.hear(/ship it/i, function(msg) {
+    return msg.send(msg.random(squirrels));
+  });
+
+  robot.respond(/hi|hello/i, function(msg) {
+    msg.send("Howdy Dan!");
+    msg.send("How are you?");
+    return;
   });
 
   // Random Example
   //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  return robot.hear(/food/i, function(msg) {
-    return msg.send(msg.random(food));
-  });
-
-  // return robot.hear(/ship it/i, function(msg) {
-  //   return msg.send(msg.random(squirrels));
-  // });
 };
