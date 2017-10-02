bb1453dcca174778e9e0b07081fff7f51ce9adb8
Dan Pitrowiski <danpitrowiski@gmail.com>
2017-10-01 23:02:18 -0700

    Moving images to come first

--- scripts/adventure-bot.js
+++ scripts/adventure-bot.js
@@ -46,46 +46,46 @@ module.exports = function(robot) {
     robot.respond(/back/i, function(msg) {
       historySkip = 1;
       loadStory();
-      msg.send(story.maintext);
+      msg.send(story.image);
+      setTimeout(function(){
+        msg.send(story.maintext);
+      },200);
       setTimeout(function(){
         msg.send(story.options);
-      },300);
-      setTimeout(function(){
-        msg.send(story.image);
       },400);
     });
 
     robot.respond(/1/i, function(msg) {
       whereAmI = whereAmI + .1;
       loadStory();
+      msg.send(story.image);
       msg.send(story.maintext);
       setTimeout(function(){
         msg.send(story.options);
       },300);
-      msg.send(story.image);
     });
 
     robot.respond(/2/i, function(msg) {
       whereAmI = whereAmI + .2;
       loadStory();
-      msg.send(story.maintext);
+      msg.send(story.image);
+      setTimeout(function(){
+        msg.send(story.maintext);
+      },200);
       setTimeout(function(){
         msg.send(story.options);
-      },300);
-      setTimeout(function(){
-        msg.send(story.image);
       },400);
     });
 
     robot.respond(/3/i, function(msg) {
       whereAmI = whereAmI + .3;
       loadStory();
-      msg.send(story.maintext);
+      msg.send(story.image);
+      setTimeout(function(){
+        msg.send(story.maintext);
+      },200);
       setTimeout(function(){
         msg.send(story.options);
-      },300);
-      setTimeout(function(){
-        msg.send(story.image);
       },400);
     });
 
@@ -111,24 +111,28 @@ function loadStory(){
       story.options = ">*1* - Keep listening\n>*2* - Turn the TV off";
       story.image = "https://media.giphy.com/media/3xz2BuuYbARSNcBBsI/giphy.gif";
       whereAmI = 2;
+      return;
     }
     if (whereAmI === 2.1){
         story.maintext = "*The woman continues talking about the virus: 'Researchers have found that the only way to kill a zombie is by destroying the brain, were the virus lives' Hmm... You don't have a weapon, and this sounds serious...*";
         story.options = ">*1* - Check the break room for something\n>*2* - Head back into the store and see what other people know";
         story.image = "https://media.giphy.com/media/9DFSyoxNE3nJS/giphy.gif";
         whereAmI = 3;
+        return;
     }
     if (whereAmI === 2.2){
         story.maintext = "*You think to yourself this is insane. What is really going on?*";
         story.options = ">*1* - Go back work\n>*2* - Keep eating, you need more time";
         whereAmI = 4;
         story.image = "https://media.giphy.com/media/3KCOFfdqmptLi/giphy.gif";
+        return;
     }
     if (whereAmI === 3.1 ){
         story.maintext = "*You look around the break room. There are plastic knives and forks. Nothing you could use as a weapon.*";
         story.options = ">*1* - Go back work\n>*2* - Call your Mom, she might know something";
         whereAmI = 5;
-        story.image = "https://media.giphy.com/media/l41lFw057lAJQMwg0/giphy.gif";
+        story.image = "https://media.giphy.com/media/l41lFw057lAJQMwg0/giphy.gif"
+        return;
     }
 
     if (whereAmI === 4.1 || whereAmI === 3.2 || whereAmI === 5.1){
@@ -136,11 +140,13 @@ function loadStory(){
         story.options = ">*1* - Ask Brad about zombies\n>*2* - Ask Joanna about zombies\n>*3* - Call your Mom";
         whereAmI = 6;
         story.image = "https://media.giphy.com/media/yoJC2qNujv3gJWP504/giphy.gif";
+        return;
     }
     if (whereAmI === 4.2){
         story.maintext = "*You eat the rest of your lunch and then start on Brad's. He'll never know. You fall asleep after your two meals... You wake up to Brad storming into the kitchen!*";
         story.options = ">*1* - Attack zombie Brad\n>*2* - Attack Brad your boss\n>*2* - Run away";
         whereAmI = 7;
         story.image = "https://media.giphy.com/media/po1HNtTPPrx5e/giphy.gif";
+        return;
     }
 }
